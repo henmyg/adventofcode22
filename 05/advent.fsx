81e432b3806b3407e3db03ccf7a6cf896d66d8e5
Henrik Top Mygind <hkmd@novonordisk.com>
2022-12-09 22:22:28 +0100

    Added F# solutions for 05 and 7

--- 05/advent.fsx
+++ 05/advent.fsx
@@ -2,11 +2,11 @@ open System.IO
 open System.Text.RegularExpressions
 
 let root = __SOURCE_DIRECTORY__
-let arr = File.ReadAllLines(Path.Join(root, "/test"))
+let arr = File.ReadAllLines(Path.Join(root, "/input"))
 let lines = Array.toList(arr)
 
-let stackInput = lines[..2]
-let moveInput = lines[5..]
+let stackInput = lines[..7] // lines[..2]
+let moveInput = lines[10..] // lines[5..]
 
 // CREATE STACKS
 let rec getStack (line:string) (i:int) :list<char> =
@@ -31,33 +31,34 @@ let rec createStacks indexes =
 
 type Stacks = list<list<char>>
 
-let indexes = [0..2]
-//let indexes = [0..8]
+// let indexes = [0..2]
+let indexes = [0..8]
 let startState = createStacks indexes
 
-let updateStacks (stacks:Stacks) fromIdx (newFrom:list<char>) toIdx (crate:char) : Stacks =
+let updateStacks (stacks:Stacks) fromIdx (newFrom:list<char>) toIdx newTo : Stacks =
     stacks
         |> List.mapi (fun i list ->
             if i = fromIdx then newFrom
-            elif i = toIdx then crate::list
+            elif i = toIdx then newTo
             else list
         )
         |> Seq.toList
 
 let printStacks stacks =
-    let debug =
-        stacks
-        |> Seq.map (fun (l:list<char>) -> 
-            l |> Seq.map (fun c -> string(c)) |> String.concat ",")
-        |> String.concat "];["
-    printfn "[%s]" debug
+    ignore ""
+    // let debug =
+    //     stacks
+    //     |> Seq.map (fun (l:list<char>) -> 
+    //         l |> Seq.map (fun c -> string(c)) |> String.concat ",")
+    //     |> String.concat "];["
+    // printfn "[%s]" debug
 
 let moveOne (stacks:Stacks) (fromIdx:int, toIdx:int) : Stacks =
     match stacks[fromIdx] with
     | crate::newFrom -> 
         printStacks stacks
-        printfn "-->"
-        let result = updateStacks stacks fromIdx newFrom toIdx crate
+        printfn "%d --> %d" fromIdx toIdx
+        let result = updateStacks stacks fromIdx newFrom toIdx (crate::stacks[toIdx])
         printStacks result
         result
             // (l |> Seq.map sprintf |> String.concat ",")))
@@ -70,12 +71,15 @@ let rec moveMany stacks fromTo (count:int) : Stacks =
         let newStacks = moveOne stacks fromTo
         moveMany newStacks fromTo (count-1)
 
+
+
+
 let doLine line stacks : Stacks =
     let regex = new Regex(@"\d+")
     let matches = regex.Matches(line) |> Seq.map (fun m -> int(m.Value)) |> Seq.toList
 
     match matches with
-    | move::fromStack::toStack::tail -> moveMany stacks (fromStack,toStack) move
+    | move::fromStack::toStack::tail -> moveMany stacks (fromStack-1,toStack-1) move
     | _ -> stacks
 
 let rec doLines lines stacks: Stacks =
@@ -85,4 +89,29 @@ let rec doLines lines stacks: Stacks =
         doLines tail newStacks
     | _ -> stacks
 
-let result = doLines moveInput startState
+let finalStacks = doLines moveInput startState
+let resultOne = finalStacks |> Seq.map (fun l -> string(l[0])) |> String.concat ""
+
+// PART TWO
+
+let moveTwo (stacks: Stacks) (fromIdx,toIdx) (count: int) : Stacks =
+    let (crates, rest) = stacks[fromIdx] |> List.splitAt count
+    updateStacks stacks fromIdx rest toIdx (crates@(stacks[toIdx]))
+
+let doLineTwo line stacks : Stacks =
+    let regex = new Regex(@"\d+")
+    let matches = regex.Matches(line) |> Seq.map (fun m -> int(m.Value)) |> Seq.toList
+
+    match matches with
+    | move::fromStack::toStack::tail -> moveTwo stacks (fromStack-1,toStack-1) move
+    | _ -> stacks
+
+let rec doLinesTwo lines stacks: Stacks =
+    match lines with
+    | line::tail ->
+        let newStacks = doLineTwo line stacks
+        doLinesTwo tail newStacks
+    | _ -> stacks
+
+let finalStacksTwo = doLinesTwo moveInput startState
+let resultTwo = finalStacksTwo |> Seq.map (fun l -> string(l[0])) |> String.concat ""
--- 07/advent.fsx
+++ 07/advent.fsx
@@ -2,29 +2,75 @@ open System.IO
 open System.Text.RegularExpressions
 
 let root = __SOURCE_DIRECTORY__
-let arr = File.ReadAllLines(Path.Join(root, "/test"))
+let arr = File.ReadAllLines(Path.Join(root, "/input"))
 let lines = Array.toList(arr)
+
+type State = {
+    dir: string list
+
+    sizes: Map<string, int>
+}
 
 let getSize (line:string) =
     int(line.Split(" ")[0])
 
-let rec parse (lines:list<string>) (dir:list<string>):int =
+let popDir (dir:list<string>) : string list =
+    match dir with
+    | head::tail -> tail
+    | _ -> dir
+
+let updateSize state (dir:string list) size : State =
+    let key = dir |> List.rev |> String.concat "/"
+
+    let sizes = state.sizes
+    let oldSize = if sizes.ContainsKey(key) then sizes.[key] else 0
+    let newSizes = sizes |> Map.add key (oldSize + size)
+    { state with sizes = newSizes }
+
+let rec updateSizes state dir size : State =
+    match dir with
+    | head::tail ->
+        let newState = updateSize state dir size
+        updateSizes newState tail size
+    | _ -> state
+
+
+let parseLine (state:State) (line:string) : State =
+    match line with
+    | s when s[..5] = "$ cd \/" -> state
+    | s when s[..3] = "$ ls" -> state
+    | s when s[..2] = "dir" -> state
+    | s when s[..6] = "$ cd .." -> 
+        { state with dir = popDir state.dir }
+    | s when s[..3] = "$ cd" ->
+        let dir = s[5..]
+        { state with dir = dir::state.dir }
+    | s when Regex.IsMatch(s, "\d.* ") ->
+        let size = getSize s
+        updateSizes state state.dir size
+
+    | _ -> state
+
+let rec parseLines state lines : State =
     match lines with
     | line::tail ->
-        match line with
-        | s when s[..5] = "$ cd \/" -> parse tail ["/"]
-        | s when s[..3] = "$ ls" -> parse tail dir
-        | s when s[..2] = "dir" -> parse tail dir
-        | s when s[..6] = "$ cd.." -> 
-            match dir with
-            | head::rest -> parse tail rest
-            | _ -> parse tail dir
-        | s when s[..4] = "$ cd" -> parse tail (s[5..]::dir)
-        | s when Regex.IsMatch(s, "\d.* ") -> getSize s + parse tail dir
-        | _ -> parse tail dir
+        let newState = parseLine state line
+        parseLines newState tail
+    | _ -> state
 
-    | _ -> 0
+let startState = { 
+    dir = []
+    sizes = Map([])
+}
 
-parse lines
+let finalStateOne = parseLines startState lines
+let resultOne = finalStateOne.sizes.Values |> Seq.filter (fun v -> v <= 100000) |> Seq.sum
 
-    
+// PART TWO
+let totalAvail = 70000000
+let needed = 30000000
+let totalUse = finalStateOne.sizes["/"]
+let freeSpace = totalAvail - totalUse
+let missing = needed - freeSpace
+
+let resultTwo = finalStateOne.sizes.Values |> Seq.filter (fun v -> v >= missing) |> Seq.min
